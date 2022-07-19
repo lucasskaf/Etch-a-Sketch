0829598262ede97e6a2417526e867887242979f2
Lucas Skaf <skaf.lucas@ufl.edu>
2022-07-19 16:22:36 -0500

    Add "canvas" (divs)

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const divGrid = document.querySelector('.grid')
 
 function generateGrid() {
-  for (let r = 0; r < 16; r++) {
-    for (let c = 0; c < 16; c++) {
+  for (let r = 0; r < 32; r++) {
+    for (let c = 0; c < 32; c++) {
       const gridSquare = document.createElement('div')
       gridSquare.setAttribute('class', 'grid-square')
       divGrid.appendChild(gridSquare)
