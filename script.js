b589afa9ba7c8e834cb1d239f1c87dbfb61ea115
Lucas Skaf <skaf.lucas@ufl.edu>
2022-07-20 16:30:47 -0500

    Add pen color changing script

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const divGrid = document.querySelector('.grid')
+const initialColor = `#000000`
 
 function generateGrid() {
   for (let r = 0; r < 32; r++) {
     for (let c = 0; c < 32; c++) {
       const gridSquare = document.createElement('div')
       gridSquare.setAttribute('class', 'grid-square')
+      gridSquare.style.backgroundColor = initialColor;
       divGrid.appendChild(gridSquare)
-
-      gridSquare.addEventListener('mouseover', () => {
-        gridSquare.style.backgroundColor = 'rgb(255, 0, 255)'
-      })
     }
   }
 }
@@ -18,14 +16,29 @@ function resetGrid() {
   const gridSquare = document.querySelectorAll('.grid-square')
   
   for(let i = 0; i < gridSquare.length; i++){
-    gridSquare[i].style.backgroundColor = 'rgb(0, 0, 0)'
+    gridSquare[i].style.backgroundColor = initialColor;
+  }
+}
+
+function changeColor(colorValue){
+  const gridSquare = document.querySelectorAll('.grid-square')
+
+  for(let i = 0; i < gridSquare.length; i++){
+    gridSquare[i].addEventListener('mouseover', () => {
+      gridSquare[i].style.backgroundColor = colorValue
+    })
   }
 }
 
 generateGrid()
 
+let colorPicker = document.querySelector('#color-picker');
 const resetButton = document.querySelector('#reset')
 
 resetButton.addEventListener('click', () => {
   resetGrid()
+})
+
+colorPicker.addEventListener('input', () => {
+  changeColor(colorPicker.value)
 })
