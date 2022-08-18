238dd3d4239f68a31dd5d62ca7dd27448edc1b55
Lucas Skaf <skaf.lucas@ufl.edu>
2022-08-18 13:33:04 -0400

    Change drawing canvas interaction

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const divGrid = document.querySelector('.grid')
-const initialColor = `#FFFFFF`
+let isMousePressed = false;
 
-function generateGrid() {
+document.body.addEventListener('mousedown', () => {
+  isMousePressed = true;
+})
+
+document.body.addEventListener('mouseup', () => {
+  isMousePressed = false;
+})
+
+function generateGrid(divGrid, initialColor) {
   for (let r = 0; r < 32; r++) {
     for (let c = 0; c < 32; c++) {
       const gridSquare = document.createElement('div')
@@ -12,11 +19,23 @@ function generateGrid() {
   }
 }
 
-function resetGrid() {
+function resetGrid(initialColor) {
   const gridSquare = document.querySelectorAll('.grid-square')
   
   for(let i = 0; i < gridSquare.length; i++){
     gridSquare[i].style.backgroundColor = initialColor;
+  }
+}
+
+function hoverColor(initialColor, colorValue){
+  const gridSquare = document.querySelectorAll('.grid-square')
+
+  for(let i = 0; i < gridSquare.length; i++){
+    gridSquare[i].addEventListener('mouseenter', () => {
+      if(isMousePressed){
+        gridSquare[i].style.backgroundColor = colorValue
+      }
+    })
   }
 }
 
@@ -30,23 +49,40 @@ function changeColor(colorValue){
   }
 }
 
-generateGrid()
+let initialColor = `#FFFFFF`
+
+const divGrid = document.querySelector('.grid')
+
+generateGrid(divGrid, initialColor)
 
 const colorPicker = document.querySelector('#color-picker');
 const resetButton = document.querySelector('#reset')
 const gridSquare = document.querySelectorAll('.grid-square')
 const eraser = document.querySelector('#eraser')
+const pencil = document.getElementById('pencil')
 
-changeColor(colorPicker.value)
+// changeColor(colorPicker.value)
 
-resetButton.addEventListener('click', () => {
-  resetGrid()
+// colorPicker.addEventListener('input', () => {
+//   color = colorPicker.value
+// })
+
+colorPicker.addEventListener('input', e => {
+  hoverColor(initialColor, colorPicker.value)
+  console.log(e)
 })
 
-colorPicker.addEventListener('input', () => {
-  changeColor(colorPicker.value)
+pencil.addEventListener('click', e => {
+  hoverColor(initialColor, colorPicker.value)
+  // changeColor(colorPicker.value)
+  console.log(e)
 })
 
 eraser.addEventListener('click', () => {
-  changeColor(initialColor)
+  hoverColor(initialColor, initialColor)
+  // changeColor(initialColor)
+})
+
+resetButton.addEventListener('click', () => {
+  resetGrid(initialColor)
 })
