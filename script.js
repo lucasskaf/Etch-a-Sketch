9a3f2cef7b9f0de02f70b2a5c31b95d4374aad5d
Lucas Skaf <skaf.lucas@ufl.edu>
2022-08-19 13:27:57 -0400

    Add eventListner to '+' display color spots

--- script.js
+++ script.js
@@ -40,6 +40,7 @@ function drawColor(initialColor, colorValue){
 }
 
 let initialColor = `#FFFFFF`
+const arrayOfColors = []
 
 const divGrid = document.querySelector('.grid')
 
@@ -50,10 +51,22 @@ const resetButton = document.querySelector('#reset')
 const gridSquare = document.querySelectorAll('.grid-square')
 const eraser = document.querySelector('#eraser')
 const pencil = document.getElementById('pencil')
+const plusColor = document.getElementById('save-color')
+const palette = document.getElementById('palette')
 
 colorPicker.addEventListener('input', e => {
   drawColor(initialColor, colorPicker.value)
   console.log(e)
+})
+
+plusColor.addEventListener('click', () => {
+  if(arrayOfColors.length <= 3){
+    arrayOfColors.push(colorPicker.value)
+    let newColorDiv = document.createElement('div')
+    newColorDiv.setAttribute('id', `color-${arrayOfColors.length}`)
+
+    palette.appendChild(newColorDiv)
+  }
 })
 
 pencil.addEventListener('click', e => {
